dd13ce726e1fba3a74638990e07a6c2f78b1761d
shashanksingh020-netizen <shashanksingh020u@gmail.com>
2026-09-21 18:50:09 +0530

    Add Week 4 assignments

---
+++ week4_assignments/q1.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class q1 {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        int left = 1;
+
+        // Product of elements on the left
+        for (int i = 0; i < n; i++) {
+            answer[i] = left;
+            left *= nums[i];
+        }
+
+        // Product of elements on the right
+        int right = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= right;
+            right *= nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        System.out.println(
+            Arrays.toString(productExceptSelf(nums))
+        );
+    }
+}
